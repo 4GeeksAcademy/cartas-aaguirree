38af8ca8bfa9b11413cb017a8d502f16461d1538
aaguirre <alejandra36a@gmail.com>
2024-07-22 11:49:16 +0000

    primer cambio

--- src/app.js
+++ src/app.js
@@ -9,3 +9,7 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const simbolos = ["♦", "♥", "♠", "♣"];
+const numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "j", "Q", "K"];
+console.log(numeros);
