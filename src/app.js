33ecc56616943f7e361dd8e9ee90a3e96ef0c132
aaguirre <alejandra36a@gmail.com>
2024-07-23 18:04:27 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -1,15 +1,39 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function actionChange() {
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumb = Math.floor(Math.random() * number.length);
+  return number[indexNumb];
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function randomSimbol() {
+  let simbol = ["spades", "diamons", "hearts", "clubs"];
+  let indexSimbol = Math.floor(Math.random() * simbol.length);
+  return simbol[indexSimbol];
+}
+let boton = document.getElementById("generador");
+boton.addEventListener("click", botonGenerar);
+
+window.onload = () => {
+  //document.querySelector(".carta").classList.add(randomSimbol());
+  document.getElementById("number").innerHTML = actionChange();
 };
 
-const simbolos = ["♦", "♥", "♠", "♣"];
-const numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "j", "Q", "K"];
-console.log(numeros);
+function botonGenerar() {
+  document.getElementById("number").innerHTML = actionChange();
+}
